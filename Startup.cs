495aaee66c8910b1296b0198ac12b9a870586035
olidias <oli.dias94@gmail.com>
2020-05-23 23:39:19 +0200

    proper bson package

--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.HttpOverrides;
-using WebApiContrib.Core.Formatter;
+using WebApiContrib.Core.Formatter.Bson;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
